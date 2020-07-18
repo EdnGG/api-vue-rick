3717393b0ba2201f0a4819157d71c168044862a1
Eden <gresseden@gmail.com>
2020-07-18 15:33:52 -0400

    first commit from develop brach

--- src/store.js
+++ src/store.js
@@ -7,9 +7,46 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    searchQuery2: ""
+    // searchQuery2: ""
+
   },
   mutations: {
+    // fetch() {
+    //   const params = {
+    //     page: this.page,
+    //     name: this.searchQuery2
+    //   };
+    //   axios
+    //     // { params } for sending type query params
+    //     //  "https://rickandmortyapi.com/api/character/?page=<>"
+    //     .get(this.URL, { params })
+    //     .then(res => {
+    //       this.characters = res.data.results;
+    //       console.log(res.data);
+    //       this.pages = res.data.info.pages;
+    //       console.log(this.pages);
+    //       console.log(` hello there ${name}`);
+    //     })
+    //     .catch(err => {
+    //       console.error(err.message);
+    //     });
+    // },
+    searchCharacter(searchQuery) {
+      this.page = 1;
+      this.fetch(), console.log(searchQuery);
+
+    },
+    test() {
+      console.log('hello')
+    },
+    getApiCharacter(searchQuery2) {
+      this.page = 1;
+      // this.search,
+      this.fetch(), console.log(searchQuery2);
+      console.log("saludos ");
+    }
+  },
+  actions: {
     fetch() {
       const params = {
         page: this.page,
@@ -29,22 +66,8 @@ const store = new Vuex.Store({
         .catch(err => {
           console.error(err.message);
         });
-    },
-    searchCharacter(searchQuery) {
-      this.page = 1;
-      this.fetch(), console.log(searchQuery);
-
-    },
-    test() {
-      console.log('hello')
-    },
-    getApiCharacter(searchQuery2) {
-      this.page = 1;
-      // this.search,
-      this.fetch(), console.log(searchQuery2);
-      console.log("saludos ");
     }
-  },
+  }
 
 
 
